f955e3f5ab7a40d96c2c35c0700e06f9026d7901
jokerCoder01 <jokercoder01@gmail.com>
2019-04-08 19:54:58 +0300

    Create and complete "Game Objects and Scripts" tutorial's project.

---
+++ CatlikeCoding/Assets/01-Basics/01-Game Objects and Scripts/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+    public bool isContinuous = false;
+
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;
+
+    void Update()
+    {
+        if (isContinuous)
+            UpdateContinuous();
+        else
+            UpdateDiscrete();
+    }
+
+    void UpdateDiscrete()
+    {
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation = Quaternion.Euler(0, time.Hour * degreesPerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, time.Minute * degreesPerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, time.Second * degreesPerSecond, 0);
+    }
+
+    void UpdateContinuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0, (float)time.TotalHours * degreesPerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, (float)time.TotalMinutes * degreesPerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, (float)time.TotalSeconds * degreesPerSecond, 0);
+    }
+}
